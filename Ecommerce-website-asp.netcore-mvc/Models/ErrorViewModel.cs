6f5dac8e5c63f8482c37da8154c82f5d83b1ba42
shahr <shahr@SH3RI>
2022-07-04 16:33:05 +0430

    Add project files.

---
+++ Ecommerce-website-asp.netcore-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ecommerce_website_asp.netcore_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
